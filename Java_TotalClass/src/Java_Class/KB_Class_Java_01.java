d1e49d45e4623d8511b0445f11341899ab1e52cf
zxcv9675 <zxcv9675@naver.com>
2025-04-14 10:17:41 +0900

    부트캠프 Day1 통합강의 (환경설정, 변수, 연산자)

---
+++ Java_TotalClass/src/Java_Class/KB_Class_Java_01.java
@@ -0,0 +1,10 @@
+// Day1 2025.04.14
+// 통합강의 : Java 1,2,3 장
+
+package Java_Class;
+
+public class KB_Class_Java_01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
