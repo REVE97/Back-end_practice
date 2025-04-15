95c261cd080e0638900582b52df1b4043d0524e6
zxcv9675 <zxcv9675@naver.com>
2025-04-15 11:47:34 +0900

    부트캠프 Day2 통합강의 (조건문, 반복문, 참조 타입)

---
+++ Java_TotalClass/src/Java_Class/KB_Class_Java_02.java
@@ -0,0 +1,9 @@
+// Day2 2025.04.15
+// 통합 강의: JAVA 4장, 5장
+// 1. 조건문 2. 반복문 3. 참조 타입
+
+// 1. Math.random() : 0.0 ~ 1.0 사이의 double 타입 난수 리턴
+package Java_Class;
+
+public class KB_Class_Java_02 {
+}
