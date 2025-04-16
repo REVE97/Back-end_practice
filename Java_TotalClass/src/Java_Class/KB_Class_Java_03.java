5f0f4caf8adace0690d4b241ca9f5c7146decdc2
zxcv9675 <zxcv9675@naver.com>
2025-04-16 13:24:12 +0900

    부트캠프 Day3 통합강의 (클래스)

---
+++ Java_TotalClass/src/Java_Class/KB_Class_Java_03.java
@@ -0,0 +1,19 @@
+// Day3 2025.04.16
+// 통합강의 : Java 6장
+// 1. 클래스
+
+package Java_Class;
+
+public class KB_Class_Java_03 {
+
+    // 필드 선언
+    String nation = "대한민국";
+    String name;
+    String ssn;
+
+    // 생성자 생성 단축키(alt+insert)
+    public KB_Class_Java_03(String name, String ssn) {
+        this.name = name;
+        this.ssn = ssn;
+    }
+}
