c3aecf329a1cadf0e00d9818d6094cc6c4cf2772
zxcv9675 <zxcv9675@naver.com>
2025-04-17 17:07:49 +0900

    스택, 힙

---
+++ Java_TotalClass/src/AfterLecture_0417/stack_hip/Data.java
@@ -0,0 +1,14 @@
+package AfterLecture_0417.stack_hip;
+
+public class Data {
+    public int value;
+
+    Data(int value) {
+        this.value = value;
+    }
+
+    //getter
+    public int getValue() {
+        return this.value;
+    }
+}
