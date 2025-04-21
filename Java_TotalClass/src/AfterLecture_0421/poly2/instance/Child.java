e408a197f0e800b3e0e92d9c578c51a2124272dd
zxcv9675 <zxcv9675@naver.com>
2025-04-21 15:26:30 +0900

    InstanceOf

---
+++ Java_TotalClass/src/AfterLecture_0421/poly2/instance/Child.java
@@ -0,0 +1,7 @@
+package AfterLecture_0421.poly2.instance;
+
+public class Child extends Parent{
+    public void childMethod() {
+        System.out.println("Child method called");
+    }
+}
---
+++ Java_TotalClass/src/AfterLecture_0421/poly2/instance/Parent.java
@@ -0,0 +1,7 @@
+package AfterLecture_0421.poly2.instance;
+
+public class Parent {
+    public void parentMethod() {
+        System.out.println("Parent method called");
+    }
+}
