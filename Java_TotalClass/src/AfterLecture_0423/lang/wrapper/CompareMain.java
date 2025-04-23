6e70dae2414608909b2db3e403fde87416182d63
zxcv9675 <zxcv9675@naver.com>
2025-04-23 17:25:20 +0900

    Wrapper 클래스

---
+++ Java_TotalClass/src/AfterLecture_0423/lang/wrapper/CompareMain.java
@@ -0,0 +1,15 @@
+package AfterLecture_0423.lang.wrapper;
+
+public class CompareMain {
+    public static void main(String[] args) {
+        Integer a = Integer.valueOf(1);
+        Integer b = Integer.valueOf(2);
+        Integer c = 10;
+        Integer d = 20;
+
+        System.out.println(a.compareTo(b));
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
